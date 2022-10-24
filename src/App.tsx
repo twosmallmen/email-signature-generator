beb94b621b2231b4b891ce12a068fb1b06161c7a
Andrew Potter <r.andrewpotter@gmail.com>
2022-10-24 12:37:06 -0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,340 @@
+import React from "react";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import {Button, Container, FormControlLabel, Switch, TextField, Typography,} from "@material-ui/core";
+//import Logo from "./assets/Logo.png";
+import Signature from "./Signature";
+import {CheckOutlined, FileCopyOutlined} from "@material-ui/icons";
+import CircularProgressWithLabel from "./CircularProgressWithLabel";
+import "./App.css";
+
+let Logo = 'https://twosmallmen.com/wp-content/uploads/2022/10/Logo.png'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            "& .MuiTextField-root": {
+                margin: theme.spacing(1),
+            },
+            "& .label-root": {
+                margin: theme.spacing(1),
+            },
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: "left",
+            color: theme.palette.text.secondary,
+        },
+        centeredImage: {
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "150px",
+            height: "150px",
+        },
+        centeredText: {
+            textAlign: "center",
+        },
+        warningIconStyle: {
+            textAlign: "center",
+            color: "#FFDC00",
+            verticalAlign: "middle",
+        },
+    })
+);
+
+export interface PhotoSignatureProps {
+    fullName: string;
+    position: string;
+    phone: string;
+    email: string;
+    photo: string;
+}
+
+interface State extends PhotoSignatureProps {
+    withPhoto: boolean;
+    copied: boolean;
+    htmlcopied: boolean;
+}
+
+const initialState: State = {
+    fullName: "",
+    position: "",
+    phone: "",
+    email: "",
+    photo: "",
+    withPhoto: false,
+    copied: false,
+    htmlcopied: false,
+};
+
+function App() {
+    const classes = useStyles();
+    const [state, setState] = React.useState<State>(initialState);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.name === "withPhoto") {
+            setState((prevState) => ({
+                ...prevState,
+                [event.target.name]: event.target.checked,
+            }));
+        } else {
+            setState((prevState) => ({
+                ...prevState,
+                [event.target.name]: event.target.value,
+            }));
+        }
+    };
+
+    const enoughData = () => {
+        let progress = 100;
+        if (state.withPhoto) {
+            if (
+                state.fullName &&
+                state.phone &&
+                state.email &&
+                state.position &&
+                state.photo
+            ) {
+                return (
+                    <React.Fragment>
+                        <Signature
+                            fullName={state.fullName}
+                            position={state.position}
+                            phone={state.phone}
+                            email={state.email}
+                            photo={state.photo}
+                        />
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+
+                        <Button
+                            disabled={state.photo.length > photoUrlMaxLength}
+                            onClick={copyToClipboard}
+                            endIcon={state.copied ? <CheckOutlined/> : <FileCopyOutlined/>}
+                        >
+                            {state.copied ? "Copied" : "Copy For Outlook"}
+                        </Button>
+
+                        <Button
+                            disabled={state.photo.length > photoUrlMaxLength}
+                            onClick={copyToClipboardHubspot}
+                            endIcon={state.htmlcopied ? <CheckOutlined/> : <FileCopyOutlined/>}
+                        >
+                            {state.htmlcopied ? "Copied" : "Copy For Hubspot"}
+                        </Button>
+        
+                    </React.Fragment>
+                );
+            } else {
+                Object.entries(state).forEach(([key, value]) => {
+                    if (
+                        ["fullName", "phone", "email", "position", "photo"].includes(key)
+                    ) {
+                        if (value.length === 0) {
+                            progress = progress - 20;
+                        }
+                    }
+                });
+            }
+        } else {
+            if (state.fullName && state.phone && state.email && state.position) {
+                return (
+                    <React.Fragment>
+                        <Signature
+                            fullName={state.fullName}
+                            position={state.position}
+                            phone={state.phone}
+                            email={state.email}
+                            photo={"https://twosmallmen.com/wp-content/uploads/2022/10/Logo.png"}
+                        />
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <Button
+                            onClick={copyToClipboard}
+                            endIcon={state.copied ? <CheckOutlined/> : <FileCopyOutlined/>}
+                        >
+                            {state.copied ? "Copied" : "Copy For Outlook"}
+                        </Button>
+                        <Button
+                            onClick={copyToClipboardHubspot}
+                            endIcon={state.htmlcopied ? <CheckOutlined/> : <FileCopyOutlined/>}
+                        >
+                            {state.htmlcopied ? "Copied" : "Copy For Hubspot"}
+                        </Button>
+                        <div></div>
+
+                    </React.Fragment>
+                );
+            } else {
+                Object.entries(state).forEach(([key, value]) => {
+                    if (["fullName", "phone", "email", "position"].includes(key)) {
+                        if (value.length === 0) {
+                            progress = progress - 25;
+                        }
+                    }
+                });
+            }
+        }
+        if (progress > 0) {
+            return (
+                <div className={classes.centeredText}>
+                    <CircularProgressWithLabel variant="determinate" value={progress}/>
+                </div>
+            );
+        } else {
+            return <div>Please, input your data</div>;
+        }
+    };
+  
+
+
+    const copyToClipboard = () => {
+        let copyText = document.querySelector(".signature");
+        const range = document.createRange();
+        if (copyText) {
+            range.selectNode(copyText);
+        }
+        const windowSelection = window.getSelection();
+        if (windowSelection) {
+            windowSelection.removeAllRanges();
+            windowSelection.addRange(range);
+        }
+        try {
+            let successful = document.execCommand("copy");
+            console.log(successful ? "Success" : "Fail");
+            setState((prevState) => ({
+                ...prevState,
+                copied: true,
+            }));
+        } catch (err) {
+            console.log("Fail");
+        }
+    };
+
+    const copyToClipboardHubspot = () => {
+        let copyText = document.querySelector(".hubspot");
+        const range = document.createRange();
+        if (copyText) {
+            range.selectNode(copyText);
+        }
+        const windowSelection = window.getSelection();
+        if (windowSelection) {
+            windowSelection.removeAllRanges();
+            windowSelection.addRange(range);
+        }
+        try {
+            let successful = document.execCommand("copy");
+            console.log(successful ? "Success" : "Fail");
+            setState((prevState) => ({
+                ...prevState,
+                htmlcopied: true,
+            }));
+        } catch (err) {
+            console.log("Fail");
+        }
+    };
+    
+    const isStateChanged = () => {
+        return JSON.stringify(state) === JSON.stringify(initialState);
+    };
+
+    const clearState = () => {
+        setState(initialState);
+    };
+
+    const photoUrlMaxLength = 1000;
+
+    return (
+        <Container>
+            <img className={classes.centeredImage} src={Logo} alt={"logo"}/>
+            <Typography variant="h2" gutterBottom className={classes.centeredText}>
+                Email Signature generator
+            </Typography>
+            <Typography
+                variant="subtitle1"
+                gutterBottom
+                className={classes.centeredText}
+            >
+                Generate Your Two Small Men Email Signature Today
+            </Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <form className={classes.root} noValidate autoComplete="off">
+                            <TextField
+                                fullWidth={true}
+                                required
+                                label="Full Name"
+                                value={state.fullName}
+                                name={"fullName"}
+                                onChange={handleChange}
+                                autoFocus={true}
+                            />
+                            <TextField
+                                fullWidth={true}
+                                required
+                                label="Position"
+                                value={state.position}
+                                name={"position"}
+                                onChange={handleChange}
+                            />
+                           
+                            <TextField
+                                fullWidth={true}
+                                required
+                                label="Telephone"
+                                value={state.phone}
+                                name={"phone"}
+                                onChange={handleChange}
+                            />
+                             <TextField
+                                fullWidth={true}
+                                required
+                                label="Email"
+                                value={state.email}
+                                name={"email"}
+                                onChange={handleChange}
+                            />
+                         
+                            {state.withPhoto && (
+                                <TextField
+                                    error={state.photo.length > photoUrlMaxLength}
+                                    fullWidth={true}
+                                    required
+                                    label="Link to image"
+                                    value={state.photo}
+                                    name={"photo"}
+                                    onChange={handleChange}
+                                    helperText={
+                                        state.photo.length > photoUrlMaxLength &&
+                                        "It's not an image url, but, probably, image in base64 form. Please, choose appropriate data."
+                                    }
+                                />
+                            )}
+                            <br/>
+                            <Button
+                                disabled={isStateChanged()}
+                                onClick={clearState}
+                                color={"secondary"}
+                            >
+                                Clear
+                            </Button>
+                            <div></div>
+                        </form>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>{enoughData()}</Paper>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
+
+export default App;
---
+++ src/Signature.tsx
@@ -0,0 +1,312 @@
+import {PhotoSignatureProps} from "./App";
+//import Logo from "./assets/Logo.png";
+
+let Logo ='https://twosmallmen.com/wp-content/uploads/2022/10/Logo.png'
+
+const Signature = (props: PhotoSignatureProps) => {
+    let instaIcon = 'https://twosmallmen.com/wp-content/uploads/2022/10/Insta-icon.png'
+    let linkedinIcon = 'https://twosmallmen.com/wp-content/uploads/2022/10/linkedin-icon.png'
+    let fbIcon = 'https://twosmallmen.com/wp-content/uploads/2022/10/facebook-icon.png'
+    let youTubeIcon = 'https://twosmallmen.com/wp-content/uploads/2022/10/youtube-icon-v2.png'
+    let instaURL = 'https://www.instagram.com/twosmallmencanada/?hl=en'
+    let linkedURL = 'https://ca.linkedin.com/company/twosmallmencanada'
+    let fbURL = 'https://www.facebook.com/twosmallmencanada/'
+    let youtubeURL = 'https://www.youtube.com/channel/UCWoGM_yzOGsOffExAW4uQ2w'
+
+
+
+
+    const signaturestyle = {
+        height: "170px", 
+        background: "#FFFFFF", 
+        display: "inline-block"
+
+      };
+
+      const mainimagestyle = {
+        width: "150px",
+        height: "150px",
+         margin: "20px"
+
+      }
+      const verticallinestyle = {
+        borderRight: "2px solid #004193",
+        height: "175px",
+        marginLeft: "10px", 
+        marginRight: "20px",
+        paddingRight: "30px",
+
+      }    
+
+      const fullnamestyle = {
+
+        fontFamily: "Bebas Neue, impact", 
+        fontSize: "40px",
+        color: "#004193",
+        lineHeight: "20px",
+        paddingTop: "15px",
+        paddingBottom: "10px",
+        marginTop:"25px",
+        marginBottom:"15px",
+      }
+
+      const positionstyle ={
+
+
+        paddingBottom: "10px",
+        fontFamily: "Bebas Neue, impact",
+        fontSize: "28px",
+        color: "#007bff",
+        lineHeight: "22px",
+      }
+
+      const phonewrapperstyle ={
+
+        marginTop: "25px",
+        display: "flex",
+        fontFamily: "Work Sans",
+
+      }
+
+      const phoneicondivstyle ={
+
+        marginRight: "5px",
+
+      }
+
+      const phoneiconstyle ={
+        width: "16px",
+    }
+
+    const emailwrapperstyle ={
+        marginTop: "5px", 
+        display: "flex",
+        fontFamily: "Work Sans",
+    }
+
+    const emailicondivstyle={
+        marginRight: "5px",
+
+    }
+
+    const sociallogosstyle={
+        marginTop: "5px",
+        width: "150px", 
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "space-between", 
+        height: "32px",
+    }
+
+    const bannerstyle={
+        marginTop: "10px",
+        display: "flex",
+    }
+
+    const height133={
+        height: "100px"
+    }
+
+    const height42={
+        height: "42px"
+    }
+
+    const width5={
+        width: "5%"
+    }
+
+    const width100={
+        width: "100%"
+    }
+    const width80={
+        width: "80%"
+    }
+
+    const pr5={
+        paddingRight: "5px"
+    }
+
+    const pt5={
+        paddingTop: "5px"
+    }
+
+    const padding0={
+        padding: "0px 0px 0px 0px"
+    }
+
+    const vAlignMiddle={
+        verticalAlign: "middle"
+    }
+
+    const width399={
+        width: "399px"
+    }
+
+    const margin0={
+        margin: "0px"
+    }
+
+    const fontSize18={
+        fontSize: "18px"
+    }
+
+    const color004193={
+        color: "#004193"
+    }
+
+    const bxZCMx={
+        margin: "0px",
+        color: "#007bff",
+        fontSize: "14px",
+        lineHeight: "16px"
+
+    }
+
+    const eQYmiW={
+        verticalAlign: "-webkit-baseline-middle",
+        fontSize: "medium",
+        fontFamily: "Arial"
+
+    }
+
+    const width556={
+        width: "556px"
+
+    }
+
+    const width30={
+        width: "30px"
+
+    }
+
+    const vAlignBottom={
+        verticalAlign: "bottom"
+    }
+
+    const bbyJzT={
+        display: "block",
+        backgroundColor: "#007bff"
+        
+    }
+
+    const color404040={
+        color: "#404040"
+    }
+
+    const iyhjGb={
+        textDecoration: "none!important",
+        color: "#404040",
+        fontSize: "12px"
+
+    }
+
+    const byigni={
+        
+        display: "inline-block",
+        backgroundColor: "transparent",
+        color: "#ffffff",
+       
+        textDecoration: "none!important",
+        lineHeight: "1",
+        fontSize: "12px",
+        borderRadius: "3px",
+        border: "initial none initial"
+
+    }
+
+    const jjNSwx={
+        textDecoration: "none!important",
+        maxWidth: "728px",
+        paddingTop: "5px"
+        
+    }
+
+    
+
+    
+
+     let htmlCopy ='<div className="signaturebody"> <div className={"signature"} style= > <table class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;" cellspacing="0" cellpadding="0"><tbody><tr><td><table style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial; width: 556px; height: 100px;" cellspacing="0" cellpadding="0"><tbody><tr style="height: 1px;"><td style="padding: 0px 0px 0px 0px; vertical-align: middle; width: 399px; height: 1px;"><h3 class="sc-fBuWsC eeihxG" style="margin: 0px; font-size: 18px; color: #004193; ">'+props.fullName+'</h3><p class="sc-fMiknA bxZCMx" style="margin: 0px; color: #007bff; font-size: 14px; line-height: 16px;">'+props.position+'</p><table class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial; padding-top: 0px;" cellspacing="0" cellpadding="0"><tbody><tr style="vertical-align: middle;"><td style="vertical-align: middle;" width="30"><table class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;" cellspacing="0" cellpadding="0"><tbody><tr><td style="vertical-align: bottom;"><span class="sc-jlyJG bbyJzT" style="display: block; background-color: #007bff;"><img class="sc-iRbamj blSEcj" style="display: block; background-color: #007bff;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png" width="13" /></span></td></tr></tbody></table></td><td style="padding: 0px; color: #404040;"><a class="sc-gipzik iyhjGb" style="text-decoration: none!important; color: #404040; font-size: 12px;" href="tel:'+props.phone+'">'+props.phone+'</a></td></tr><tr style="vertical-align: middle;"><td style="vertical-align: middle;" width="30"><table class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;" cellspacing="0" cellpadding="0"><tbody><tr><td style="vertical-align: bottom;"><span class="sc-jlyJG bbyJzT" style="display: block; background-color: #007bff;"><img class="sc-iRbamj blSEcj" style="display: block; background-color: #007bff;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png" width="13" /></span></td></tr></tbody></table></td><td style="padding: 0px;"><a class="sc-gipzik iyhjGb" style="text-decoration: none!important; color: #000000; font-size: 12px;" href="mailto:'+props.email+'@twosmallmen.com">'+props.email+'@twosmallmen.com</a></td></tr><tr style="vertical-align: middle;"><td style="vertical-align: middle;" width="30"><table class="sc-gPEVay eQYmiW" style="vertical-align: -webkit-baseline-middle; font-size: medium; font-family: Arial;" cellspacing="0" cellpadding="0"><tbody><tr><td style="vertical-align: bottom;"><span class="sc-jlyJG bbyJzT" style="display: block; background-color: #007bff;"><img class="sc-iRbamj blSEcj" style="display: block; background-color: #007bff;" src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png" width="13" /></span></td></tr></tbody></table></td><td style="padding: 0px;"><a class="sc-gipzik iyhjGb" style="text-decoration: none!important; color: #000000; font-size: 12px;" href="https://twosmallmen.com">twosmallmen.com</a></td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td><a class="sc-fAjcbJ byigni" style="display: inline-block; background-color: transparent; color: #ffffff; font-weight: bold; text-decoration: none!important; text-align: center; line-height: 1; font-size: 12px; border-radius: 3px; border: initial none initial;" href="https://twosmallmen.com/about-us/giving-back/" target="_blank" rel="noopener noreferrer"><img class="sc-caSCKo jjNSwx" style="text-decoration: none!important; max-width: 728px; padding-top: 5px;" role="presentation" src="https://twosmallmen.com/wp-content/uploads/2022/10/TSM-Email-Banner-6.png" /></a></td></tr></tbody></table>'
+  
+
+
+    return (
+    <div className="signaturebody">
+                <div className={"signature"} style={signaturestyle} >
+
+                <table className="sc-gPEVay eQYmiW" style={eQYmiW} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr>
+<td>
+<table style={{...eQYmiW,...width556,...height133}} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr style={height133}>
+<td style={{...padding0, ...vAlignMiddle,...width399,...height133}}>
+<h3 className="sc-fBuWsC eeihxG" style={{...margin0,...fontSize18,...color004193}}>{props.fullName}</h3>
+<p className="sc-fMiknA bxZCMx" style={bxZCMx}>{props.position}</p>
+<table className="sc-gPEVay eQYmiW" style={eQYmiW} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr style={vAlignMiddle}>
+<td style={{...vAlignMiddle,...width30}}>
+<table className="sc-gPEVay eQYmiW" style={eQYmiW} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr>
+<td style={vAlignBottom}><span className="sc-jlyJG bbyJzT" style={bbyJzT}><img className="sc-iRbamj blSEcj" style={bbyJzT} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style={{...padding0, ...color404040}}><a className="sc-gipzik iyhjGb" style={iyhjGb} href="tel:(250) 498-3996">(250) 498-3996</a></td>
+</tr>
+<tr style={vAlignMiddle}>
+<td style={{...vAlignMiddle, ...width30}}>
+<table className="sc-gPEVay eQYmiW" style={eQYmiW} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr>
+<td style={vAlignBottom}><span className="sc-jlyJG bbyJzT" style={bbyJzT}><img className="sc-iRbamjblSEcj" style={bbyJzT} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style={padding0}><a className="sc-gipzik iyhjGb" style={iyhjGb} href="mailto:'+{props.email}+'">{props.email}@twosmallmen.com</a></td>
+</tr>
+<tr style={vAlignMiddle}>
+<td style={{...vAlignMiddle, ...width30}}>
+<table className="sc-gPEVay eQYmiW" style={eQYmiW} cellSpacing="0" cellPadding="0">
+<tbody>
+<tr>
+<td style={vAlignBottom}><span className="sc-jlyJG bbyJzT" style={bbyJzT}><img className="sc-iRbamj blSEcj" style={bbyJzT} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png" width="13" /></span></td>
+</tr>
+</tbody>
+</table>
+</td>
+<td style={padding0}><a className="sc-gipzik iyhjGb" style={iyhjGb} href="https://twosmallmen.com">twosmallmen.com</a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td><a className="sc-fAjcbJ byigni" style={byigni} href="https://twosmallmen.com/about-us/giving-back/" target="_blank" rel="noopener noreferrer"><img className="sc-caSCKo jjNSwx" style={jjNSwx} role="presentation" src="https://twosmallmen.com/wp-content/uploads/2022/10/TSM-Email-Banner-6.png" /></a></td>
+</tr>
+</tbody>
+</table>
+                    
+       
+
+        </div>
+        <div className="htmlcopy">
+<div className="hubspot">{htmlCopy}</div>
+</div>
+        </div>
+       
+        
+        
+
+    );
+};
+
+export default Signature;
